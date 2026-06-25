40a2b28d196109ca5bc022558fee61a128db3e45
dhruvaravind7 <dhruvaravind@hotmail.com>
2026-06-25 14:59:33 -0400

    Removed dataset

--- Model_Testing/model_testing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-n_train = pd.DataFrame("Bank_Customer_Churn_Dataset/N_train.npy")
-c_train = pd.DataFrame("Bank_Customer_Churn_Dataset/C_train.npy")
-
-n_train.head()
