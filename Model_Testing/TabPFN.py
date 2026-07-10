50b2081b09ecd46a1e70dd2397dd58e65d8d3e20
dhruvaravind7 <dhruvaravind@hotmail.com>
2026-07-10 15:05:58 -0400

    Added another dataset and tested on that

--- Model_Testing/TabPFN.py
+++ Model_Testing/TabPFN.py
@@ -3,22 +3,27 @@ import os
 import numpy as np
 import pandas as pd
 import time
+import matplotlib
+matplotlib.use("Qt5Agg")
+import matplotlib.pyplot as plt
+import addcopyfighandler
 
+from sklearn.calibration import calibration_curve
 from dotenv import load_dotenv
 from tabpfn_client import TabPFNClassifier
-from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix, matthews_corrcoef, cohen_kappa_score, average_precision_score
+from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix, matthews_corrcoef, cohen_kappa_score, average_precision_score, brier_score_loss
 
 # Sets the access token for the TabPFN API
 load_dotenv()
 tabpfn_client.set_access_token(os.getenv("TABPFN_TOKEN"))
 
 # Loads the training and testing data
-train = pd.read_csv("/Users/dhruvaravind/Desktop/Work/WoodWide/Model_Testing/Bank_Marketing_Dataset/marketing_train.csv")
-test = pd.read_csv("/Users/dhruvaravind/Desktop/Work/WoodWide/Model_Testing/Bank_Marketing_Dataset/marketing_test.csv")
-X_train = train.drop(columns=["Subscribed"])
-y_train = train["Subscribed"]
-X_test = test.drop(columns=["Subscribed"])
-y_test = test["Subscribed"]
+train = pd.read_csv("/Users/dhruvaravind/Desktop/Work/WoodWide/Model_Testing/Bank_Churn_Dataset/bank_train.csv")
+test = pd.read_csv("/Users/dhruvaravind/Desktop/Work/WoodWide/Model_Testing/Bank_Churn_Dataset/bank_test.csv")
+X_train = train.drop(columns=["Exited"])
+y_train = train["Exited"]
+X_test = test.drop(columns=["Exited"])
+y_test = test["Exited"]
 
 # Trains the model
 training_start = time.time()
@@ -41,3 +46,24 @@ print("Confusion Matrix:\n", confusion_matrix(y_test, test_preds), "\n")
 print("Training time taken: ", testing_start - training_start, " seconds", "\n")
 print("Testing time taken: ", time.time() - testing_start, " seconds", "\n")
 print("Total time taken: ", time.time() - training_start, " seconds", "\n")
+
+true_prob, pred_prob = calibration_curve(y_test, test_probs, n_bins=10, strategy="quantile")
+brier = brier_score_loss(y_test, test_probs)
+print(f"Brier Score Loss: {brier:.4f}")
+plt.figure(figsize=(8, 6))
+
+# Plot the ideal baseline (perfect calibration)
+plt.plot([0, 1], [0, 1], linestyle="--", color="gray", label="Perfectly Calibrated")
+
+# Plot the model's actual calibration curve
+plt.plot(pred_prob, true_prob, marker="o", color="blue", label="TabPFN")
+
+# Formatting the visual graph
+plt.xlabel("Mean Predicted Probability")
+plt.ylabel("Fraction of Positives (Actual Frequency)")
+plt.title("TabPFN Calibration Curve")
+plt.legend(loc="upper left")
+plt.grid(True)
+
+# Display the plot
+plt.show()
